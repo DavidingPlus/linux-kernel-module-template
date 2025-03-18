4376154277760a3d639c1539f0a4f8102243cca8
DavidingPlus <davidingplus@qq.com>
2025-03-18 15:21:22 +0000

    add unit test support, more info at https://github.com/DavidingPlus/linux-kernel-module-template/issues/1

---
+++ test/xmake.lua
@@ -0,0 +1,14 @@
+-- conan needs pip(by apt) and cmake(by snap recommended)
+add_requires("conan::gtest 1.12.1", {alias = "gtest"})
+
+
+target("tests")
+    set_filename ("gtest-run")
+
+    set_kind ("binary")
+    add_files ("*.cpp")
+    add_packages ("gtest")
+    set_languages ("c++11")
+
+    set_default (false)
+    set_targetdir ("$(buildir)/$(plat)/$(arch)/$(mode)/test/")
